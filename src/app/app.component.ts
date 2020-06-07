76661814a3eb46f4c0207ed367c76fc497d2e6e0
Luis Yauri <david.yauri147@gmail.com>
2020-06-07 17:30:24 -0500

    Add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'kirusafe';
+  listMenu = [
+    {
+      title: 'Inicio'
+    },
+    {
+      title: '¿Quienes somos?'
+    },
+    {
+      title: '¿Quienes somos?'
+    },
+    {
+      title: '¿Quienes confian en nosotros?'
+    }
+  ];
+  logoKirusafe = 'assets/img/logo.png'
+  headerContent = {
+    title: 'KiruSafe',
+    subTitle: '¡El Lazo que nos une!',
+    description: 'Red de voluntarios y docentes a nivel nacional enfocados en promover la salud oral.',
+    btn: 'VER VIDEO',
+    img: 'assets/img/img_girls.png'
+  }
+
+  ngOnInit() {
+  }
+
+
 }
