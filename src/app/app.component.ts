529068ed3ed12ec9d2425202e247f6c745908446
Luis Yauri <david.yauri147@gmail.com>
2020-06-07 19:17:26 -0500

    Add section two

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,8 +34,27 @@ export class AppComponent implements OnInit{
     description: 'Somos una comunidad que utiliza redes sociales libres como Facebook para realizar asesorías grupales por colegio y a su vez por aula; aprovechando que cada maestra conoce la realidad de sus niños y su familia, se brinda educación en el marco  promoción - prevención en niños menores de 12 años; tocando temas de salud, especialmente de salud bucal.',
     img: 'assets/img/img_section_1.png'
   }
+  sectionTwo = {
+    title: '¿Como lo hacemos?',
+    listItem: [
+      {
+        description: 'Brindar educación en la salud en el marco de la promoción y prevención, utilizando Facebook para interactuar y transmitir los conocimientos.',
+        img: 'assets/img/img_section_2_1.png',
+        flag: false
+      },
+      {
+        description: 'Además también birndamos una solución clínica para cada caso, con encargados de KiruSafe  que agendarán una Teleconsulta con un especialista y/o estudiante capacitado para brindar alternativas de solución y/o un plan de tratamiento.',
+        img: 'assets/img/img_section_2_2.png',
+        flag: false
+      }
+    ]
+  }
 
   ngOnInit() {
+  }
+
+  checkBox(item) {
+    item.flag = !item.flag
   }
 
 
