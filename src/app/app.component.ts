f8a4f9cae9963d3536574cae8040534b673b44a6
Luis Yauri <david.yauri147@gmail.com>
2020-06-09 07:47:36 -0500

    Add date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,16 @@ export class AppComponent implements OnInit{
   title = 'kirusafe';
   listMenu = [
     {
-      title: 'Inicio'
+      title: 'Inicio',
+      id: ''
     },
     {
-      title: '¿Quiénes somos?'
+      title: '¿Quiénes somos?',
+      id: 'section-1'
     },
     {
-      title: '¿Cómo lo hacemos?'
+      title: '¿Cómo lo hacemos?',
+      id: 'section-2'
     }
   ];
   urlVideo = 'https://www.youtube.com/watch?v=Q4u0Q69gmLM'
@@ -25,25 +28,25 @@ export class AppComponent implements OnInit{
   headerContent = {
     title: 'KiruSafe',
     subTitle: '¡El Lazo que nos une!',
-    description: 'Red de voluntarios y docentes a nivel nacional enfocados en promover la salud oral.',
+    description: 'Red nacional de voluntariado en Odontología enfocados en la promoción y prevención en salud oral',
     btn: 'VER VIDEO',
     img: 'assets/img/img_girls.png'
   }
   sectionOne = {
     title: '¿Quiénes somos?',
-    description: 'Somos una comunidad que utiliza redes sociales libres como Facebook para realizar asesorías grupales por colegio y a su vez por aula; aprovechando que cada maestra conoce la realidad de sus niños y su familia, se brinda educación en el marco  promoción - prevención en niños menores de 12 años; tocando temas de salud, especialmente de salud bucal.',
+    description: 'Somos una comunidad que integra docentes Especialistas y estudiantes de diversas profesiones, con la finalidad de brindar educación en el marco de la promoción y prevención de la salud, con mayor énfasis en la salud bucal  de  niños menores a 12 años de zonas rurales, para el  cual empleamos metodologías ágiles y medios de comunicación digital de acceso libre y así poder desarrollar sesiones educativas personalizadas por cada grado y sección, teniendo siempre como aliado estratégico.',
     img: 'assets/img/img_section_1.png'
   }
   sectionTwo = {
     title: '¿Cómo lo hacemos?',
     listItem: [
       {
-        description: 'Brindar educación en la salud en el marco de la promoción y prevención, utilizando Facebook para interactuar y transmitir los conocimientos.',
+        description: 'Brindamos una educación en la salud en el marco de la promoción y prevención, utilizando Facebook para interactuar y transmitir los conocimientos.',
         img: 'assets/img/img_section_2_1.png',
         flag: false
       },
       {
-        description: 'Además también birndamos una solución clínica para cada caso, con encargados de KiruSafe  que agendarán una Teleconsulta con un especialista y/o estudiante capacitado para brindar alternativas de solución y/o un plan de tratamiento.',
+        description: 'Además también brindamos una solución clínica para cada caso, con encargados de KiruSafe  que agendarán una Teleconsulta con un especialista y/o estudiante capacitado para brindar alternativas de solución y/o un plan de tratamiento.',
         img: 'assets/img/img_section_2_2.png',
         flag: false
       }
@@ -82,5 +85,9 @@ export class AppComponent implements OnInit{
 
   openLink(url) {
     window.open(url, "_blank");
+  }
+
+  redirect(id) {
+    location.href= `#${id}`;
   }
 }
